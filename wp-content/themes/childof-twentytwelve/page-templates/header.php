4e343d501f55ca0af700aa0574e9e4aa2d343ade
Angelina Gutierrez <angelina.ogutierrez@gmail.com>
2018-04-10 17:41:15 -0400

    Replaced Google Analytics code in proper places

--- wp-content/themes/childof-twentytwelve/page-templates/header.php
+++ wp-content/themes/childof-twentytwelve/page-templates/header.php
@@ -29,6 +29,16 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117337478-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-117337478-1');
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
